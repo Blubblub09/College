f200d44b932d376fb370cde4036c9b917c545ace
Blubblub09 <124732911+Blubblub09@users.noreply.github.com>
2025-09-30 13:01:47 +0000

    Added comments and begun task 3

--- Numbers.py
+++ Numbers.py
@@ -1,3 +1,6 @@
+#Asks for teas and coffees sold
 teas = int(input("How many teas? "))
 coffees = int(input("How many coffees? "))
+
+#Prints income
 print("Total income is £"+str(teas*2 + coffees*3))
--- Text.py
+++ Text.py
@@ -1,3 +1,6 @@
+#Asks for book title
 book = input("Enter the title of a book \n")
+
+# Prints title info
 print("The title has", len(book) ,"characters. \n"
 "The title in uppercase:", str.upper(book))
