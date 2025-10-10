e4941fb491fb4948d3b4a7cbcc8f1c9df27800ec
Blubblub09 <124732911+Blubblub09@users.noreply.github.com>
2025-10-10 10:57:29 +0000

    renamed word to target for  better readablility

--- PythonTasks/Week 5/Truedle.py
+++ PythonTasks/Week 5/Truedle.py
@@ -2,10 +2,10 @@ import random
 count = 0
 repeat = 1
 words = ["apple", "horse"]
-word = random.choice(words)
+target = random.choice(words)
 
 #defines the function for the game turn
-def wordle(word):
+def wordle(target):
     global count
     global repeat
     result = "XXXXX"
@@ -15,37 +15,37 @@ def wordle(word):
 
     #checks matches
     for letter in range(len(guess)):
-        #Checks every letter in the word for matches before checking other conditions to prevent erroneous results
-        if guess[letter] == word[letter]:
+        #Checks every letter in the target for matches before checking other conditions to prevent erroneous results
+        if guess[letter] == target[letter]:
             #marks the letter as correct
             result = result[:letter] + "G" + result[letter+1:]
-            #removes letter from word to ensure it cannot be used for later comparisons
-            word = word[:letter] + "_" + word[letter+1:]
+            #removes letter from target to ensure it cannot be used for later comparisons
+            target = target[:letter] + "_" + target[letter+1:]
 
     #checks incorrect positions and 
     for letter in range(len(guess)):
         #Checks that this letter is not already marked as correct
-        if word[letter] != "_":
-            #checks if letter is in the word if not in the same position
-            if guess[letter] in word:
+        if target[letter] != "_":
+            #checks if letter is in the target word if not in the same position
+            if guess[letter] in target:
                 #marks the letter as in the wrong position
                 result = result[:letter] + "Y" + result[letter+1:]
-                word = word[:word.index(guess[letter])] + "_" + word[word.index(guess[letter])+1:]
-            #resorts to narking the letter as incorrect if not present in the word
-            elif word[letter] != "_":
+                target = target[:target.index(guess[letter])] + "_" + target[target.index(guess[letter])+1:]
+            #resorts to marking the letter as incorrect if not present in the target word
+            elif target[letter] != "_":
                 result = result[:letter] + "X" + result[letter+1:]
 
     #prints the result of the user's guess
     print(result)
 
-    #checks if the user has correctly guessed the word
-    if word == "_____":
-        print(f"Congratulations!\n You found the correct word in {count} attempts!")
+    #checks if the user has correctly guessed the target word
+    if target == "_____":
+        print(f"Congratulations!\n You found the correct target in {count} attempts!")
         repeat = 0
 
 #Makes the turns repeat
 while repeat == 1:
-    wordle(word)
+    wordle(target)
 
     if repeat == 0:
         choice = int(input("Do you want to play again? \n"
@@ -55,6 +55,6 @@ while repeat == 1:
         if choice == 1:
             repeat = 1
             print("Good luck!")
-            word = random.choice(words)
+            target = random.choice(words)
         else:
             print("Goodbye!")
